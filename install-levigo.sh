607db33c92ccf71f862010e671fd4ad945059cbd
Frank Xu <frank.xu@northbright.com>
2016-04-07 11:29:07 +0800

    Fix -I and -L path error

--- install-levigo.sh
+++ install-levigo.sh
@@ -4,7 +4,7 @@ wget https://github.com/google/leveldb/archive/master.zip
 unzip master.zip
 cd leveldb-master
 make
-CGO_CFLAGS="-I./leveldb-master/include" CGO_LDFLAGS="-L./leveldb-master/out-shared -lsnappy" go get github.com/jmhodges/levigo
+CGO_CFLAGS="-I./include" CGO_LDFLAGS="-L./out-shared -lsnappy" go get github.com/jmhodges/levigo
 
 
 
